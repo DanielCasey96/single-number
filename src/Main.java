b23bce98ae80fb7caa4861a94b05aee931757509
spoona96 <dan_spooner15@hotmail.co.uk>
2022-07-02 17:29:55 +0100

    I actually cry everytime
    Runtime: 2374 ms, faster than 5.00% of Java online submissions for Single Number.
    Memory Usage: 54.7 MB, less than 7.48% of Java online submissions for Single Number.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.singleNumber(new int[]{1, 23, 54, 65});
+        main.singleNumber(new int[]{1, 2, 1, 3, 3});
     }
 
     public int singleNumber(int[] nums) {
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int value = 0;
+        for(int i = 0; i<nums.length; i++) {
+            value = value + nums[i];
+            if(numbers.contains(nums[i])) {
+                value = value - nums[i] * 2;
+            }
+            numbers.add(i, nums[i]);
+        }
+       return value;
     }
 }
