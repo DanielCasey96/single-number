1d02fe903c010dbcaa1cbe4aff10c4957a5ebcbd
spoona96 <dan_spooner15@hotmail.co.uk>
2022-07-02 16:38:19 +0100

    no message

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.singleNumber(new int[]{1, 23, 54, 65});
+    }
+
+    public int singleNumber(int[] nums) {
+
+    }
+}
