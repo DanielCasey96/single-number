867a4cfd1ab59aa18353033a7ff41eb779f4796a
spoona96 <dan_spooner15@hotmail.co.uk>
2022-07-02 19:10:06 +0100

    Added a significantly better second method Was the way i wanted to do it initally but brain no work

--- src/Main.java
+++ src/Main.java
@@ -21,4 +21,25 @@ public class Main {
         }
        return value;
     }
+    //2000ms runtime
+    //54.8MB memory usage
+
+    public int singleNumber2(int[] nums) {
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                return nums[i];
+            }
+            count = 0;
+        }
+        return -1;
+    }
+    //498ms runtime
+    //50.4MB memory usage
 }
